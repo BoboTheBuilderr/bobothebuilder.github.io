1ff33f5255a63b4e9c970ca07f235a6554d04ba7
BoboTheBuilderr <130607755+BoboTheBuilderr@users.noreply.github.com>
2024-01-02 09:26:16 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -82,9 +82,6 @@ function aboutUsContent() {
 	// Contact Form content function
 function contactFormContent() {
     return `
-       
-
-
 	<form action="https://formspree.io/f/moqgqwvl" method="POST">           
  <!-- Left side items -->
             <div class="form-item">
@@ -171,35 +168,33 @@ function loadContent() {
             break;
     }
 }
-var form = document.getElementById("Contact-Us");
+var form = document.getElementById("ContactUs");
 async function handleSubmit(event) {
-event.preventDefault();
-var status = document.getElementById("my-form-status");
-var data = new FormData(event.target);
-fetch(event.target.action, {
-  method: form.method,
-  body: data,
-  headers: {
-    'Accept': 'application/json'
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwnProperty(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form";
+                }
+            });
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+    });
 }
-}).then(response => {
-  if (response.ok) {
-    status.innerHTML = "Thanks for your submission!";
-    form.reset()
-  } else {
-    response.json().then(data => {
-    if (Object.hasOwn(data, 'errors')) {
-      status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-    } else {
-      status.innerHTML = "Oops! There was a problem submitting your form"
-    }
-  })
-}
-}).catch(error => {
-  status.innerHTML = "Oops! There was a problem submitting your form"
-});
-}
-form.addEventListener("submit", handleSubmit)
-// Function to handle contact form submission
+form.addEventListener("submit", handleSubmit);
 
-}
